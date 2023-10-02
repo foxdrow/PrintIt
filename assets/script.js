0c1afdb2bdb26783ad2052e9d8c141f81582de88
Raphael <rapha.saverys@gmail.com>
2023-10-02 12:38:03 +0200

    create dots eventListener & changeImage function

--- assets/script.js
+++ assets/script.js
@@ -18,8 +18,26 @@ const slides = [
   },
 ];
 
+let banner_img = document.querySelector(".banner-img");
+let banner_tagLine = document.querySelector("#banner p");
 const arrow_left = document.querySelector(".arrow_left");
 const arrow_right = document.querySelector(".arrow_right");
+let dot = document.querySelectorAll(".dot");
+
+for (let i = 0; i < dot.length; i++) {
+  dot[i].addEventListener("click", () => {
+    changeImage(i);
+  });
+}
+
+changeImage = (nb) => {
+  banner_img.src = "./assets/images/slideshow/" + slides[nb].image;
+  banner_tagLine.innerHTML = slides[nb].tagLine;
+  dot.forEach((d) => {
+    d.classList.remove("dot_selected");
+  });
+  dot[nb].classList.add("dot_selected");
+};
 
 arrow_left.addEventListener("click", function () {
   alert("left");
